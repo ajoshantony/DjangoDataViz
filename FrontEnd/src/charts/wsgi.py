014c1c7d11ebf1adb1c1e0382f9bc127469a976a
ajojothepineapple <ajojothepineapple@gmail.com>
2018-05-04 18:50:11 -0700

    make it better

--- FrontEnd/src/charts/wsgi.py
+++ FrontEnd/src/charts/wsgi.py
@@ -7,19 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charts.settings")
-#
-# # application = get_wsgi_application()
-# application = Cling(get_wsgi_application())
-
-
 import os
+
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charts.settings")
-application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
