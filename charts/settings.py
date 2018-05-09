e3bfd24aa040c73617e6f61136d7e6865a58f1e1
ajojothepineapple <ajojothepineapple@gmail.com>
2018-05-08 19:57:21 -0700

    changing line 23 in settings.py

--- charts/settings.py
+++ charts/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'y=c(8s0+^xa4%ak65n$hutr=n4hy4%*h+%34-*#p(9iou0+j(6'
+SECRET_KEY = 'y=c(8s0+^xa4%ak65n$hutr=n4hy4%*h+%34-*#p(9iou0+j(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = config('SECRET_KEY')
